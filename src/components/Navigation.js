f427c6e54985e04f93d1f801dbcc55158f677063
Elyas <touahria.elyas@outlook.com>
2022-02-02 22:01:40 +0100

    starting project

---
+++ src/components/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navigation = () => {
+  return (
+    <div className='navigation'>
+      <NavLink end to="/" className={({ isActive }) => "nav-link" + (isActive ? " activated" : "")}>Home</NavLink>
+      <NavLink end to="/about" className={({ isActive }) => "nav-link" + (isActive ? " activated" : "")}>About</NavLink>
+    </div>
+  );
+};
+
+export default Navigation;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import Navigation from "../components/Navigation";
+
+const Home = () => {
+  return(
+    <>
+    <Navigation />
+      <h1>Bismillah</h1>
+    </>
+  )
+};
+
+export default Home;
